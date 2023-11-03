7272d3fae14c2e6c56bdb367ca52d65a7e4c7da2
alumno <alumno@Instituto.local>
2023-11-03 08:17:07 -0300

    primeros cambios

---
+++ valen.js
@@ -0,0 +1,32 @@
+//1
+let nombre = prompt('ingresa tu nombre')
+alert ('bienvenido ' +  nombre)
+
+//2
+let num1 = 9
+let num2 = 3
+
+alert (num1 + num2)
+alert (num1 / num2)
+alert (num1 * num2)
+alert (num1 - num2)
+
+//3
+let numero1 = prompt('ingresa un numeros')
+let numero2 = prompt('ingresa otro numero')
+
+alert (numero1 + numero2)
+alert (numero1 / numero2)
+alert (numero1 * numero2)
+alert (numero1 - numero2)
+
+//4 
+let cuota= prompt('ingrese la cantidad de cuotas')
+let diner= prompt('pon el monto')
+alert (diner / cuota)+ ('el total a pagar es')
+
+//5
+let sueldo= prompt ('cuanto cobras')
+let alquier= prompt ('cuanto vale el alquier')
+let auto= prompt ('cuanto vale las cuotas para el auto')
+alert (sueldo + alquiler + auto)
